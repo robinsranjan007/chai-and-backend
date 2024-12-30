ef2fba70c0c2395e8d66a4fcb8e99fa127991824
robinsranjan007 <robinsranjan619@gmail.com>
2024-12-30 02:02:09 -0500

    cloudinary

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,37 @@
+import { v2 as cloudinary } from 'cloudinary';
+import fs from 'fs'
+
+ 
+    // Configuration
+    cloudinary.config({ 
+        cloud_name: process.env.COUDINARY_CLOUD_NAME, 
+        api_key: process.env.CLOUDINARY_API_KEY, 
+        api_secret: process.env.CLOUDINARY_API_SECERET
+    });
+
+
+
+    const uploadOnCloudinary = async (localfilepath)=>{
+        try{
+            if(!localfilepath) return null
+            
+        //upload the file on the cloudinary
+
+    const response=  await  cloudinary.uploader.upload(localfilepath,{
+            resource_type:"auto"
+
+        })
+            //file has been uploaded successfully 
+            console.log("file is uploaded on cloudinary",response.url)
+            return response;
+
+        }catch(error){
+
+            fs.unlinkSync(localfilepath) //remove locally saved temporary file as the upload operation failed
+            return null;
+
+        }
+    }
+
+
+    export {uploadOnCloudinary}
